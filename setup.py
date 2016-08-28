5fcf1af59fe6c6c720851d1cba9e99e8b99c2395
XiaochenCui <jcnlcxc@163.com>
2016-08-28 14:42:08 +0800

    update

--- setup.py
+++ setup.py
@@ -3,5 +3,9 @@ from distutils.core import setup
 setup(
     name='tools',
     version='0.1',
-    packages=['tools'],
+    description='CuiXiaochen\'s toolkit',
+    author='Cui Xiaochen',
+    author_email='jcnlcxc@163.com',
+    url='https://github.com/XiaochenCui/tools_cxc',
+    packages=['tools','tools.data_structure'],
 )
---
+++ tools/sort.py
@@ -0,0 +1,39 @@
+import operator
+from collections import defaultdict
+
+from functools import cmp_to_key
+
+
+# sort a dict by key and return a sorted list
+def sort_key(dic, reverse=False):
+    assert type(dic) in{dict,defaultdict}
+    sorted_dic = sorted(dic.items(), key=operator.itemgetter(0), reverse=reverse)
+    return sorted_dic
+
+
+# sort a dict by value and return a sorted list
+def sort_val(dic, reverse=False):
+    assert type(dic) in{dict,defaultdict}
+    sorted_dic = sorted(dic.items(), key=operator.itemgetter(1), reverse=reverse)
+    return sorted_dic
+
+
+# sort a dict by len of key and return a sorted list
+def sort_len_key(dic, reverse=False):
+    assert type(dic) in{dict,defaultdict}
+    sorted_dic = sorted(dic, key=lambda k: len(k), reverse=reverse)
+    return [(i, dic[i]) for i in sorted_dic]
+
+
+# sort a dict by len of value and return a sorted list
+def sort_len_val(dic, reverse=False):
+    assert type(dic) in{dict,defaultdict}
+    sorted_dic = sorted(dic, key=lambda k: len(dic[k]), reverse=reverse)
+    return [(i, dic[i]) for i in sorted_dic]
+
+
+if __name__ == '__main__':
+    d = {"one": [(1, 3), (1, 4)], "two": [(1, 2), (1, 2), (1, 3)], "three": [(1, 1)]}
+    d_1 = {(1, 2, 3, 4): 1, (1, 2): 2, (1, 2, 3, 4, 5): 3}
+    print(sort_len_val(d))
+    print(sort_len_key(d_1))
